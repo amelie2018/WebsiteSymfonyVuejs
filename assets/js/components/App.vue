cbe1bf57d872d5713a985da22565955e683e9d73
Amel Tlili <ameltlili7@gmail.com>
2020-11-21 11:39:57 +0100

    21/11/2020 11:39 commit

--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -14,7 +14,10 @@
        <router-link class="nav-link pr-3" to="/">acceuil</router-link>
       </li>
       <li class="nav-item">
-      
+       <!---------------
+<li class="item-108  deeper parent"><a href=""> Présentation Ville</a><span class="offcanvas-menu-toggler collapsed" data-toggle="collapse" data-target="#collapse-menu-108"><i class="open-icon fa fa-angle-down"></i><i class="close-icon fa fa-angle-up"></i></span><ul class="collapse" id="collapse-menu-108"><li class="item-131"><a href="/index.php?option=com_content&amp;view=article&amp;id=8&amp;Itemid=131&amp;lang=fr"> Situation géographique</a></li><li class="item-132"><a href="/index.php?option=com_content&amp;view=article&amp;id=18&amp;Itemid=132&amp;lang=fr"> Présentation en Bref</a></li><li class="item-134"><a href="/index.php?option=com_content&amp;view=article&amp;id=7&amp;Itemid=134&amp;lang=fr"> La ville en chiffres</a></li><li class="item-135"><a href="/index.php?option=com_content&amp;view=article&amp;id=9&amp;Itemid=135&amp;lang=fr"> Histoire de la ville</a></li><li class="item-137"><a href="/index.php?option=com_content&amp;view=article&amp;id=11&amp;Itemid=137&amp;lang=fr"> Activité économique</a></li><li class="item-293"><a href="/index.php?option=com_content&amp;view=article&amp;id=46&amp;Itemid=293&amp;lang=fr"> Annuaire de la Ville</a></li><li class="item-373"><a href="/index.php?option=com_content&amp;view=article&amp;id=252&amp;Itemid=373&amp;lang=fr"> Plan d'management de benarous</a></li></ul></li>
+-->
+    
         <router-link class="nav-link pr-3" to="/services">Services</router-link>
       </li>
       <li class="nav-item">
@@ -37,12 +40,15 @@
      
     </nav>
     </div>
+    <div>
+       <Slider />
+    </div>
   
 
   <!-- Main -->
     <div class="App">
           <router-view />
-           <Slider />
+          
     </div>
     
  
